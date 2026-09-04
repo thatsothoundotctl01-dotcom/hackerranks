9049bd6be6e87151ad83a79aa9810216b06cafdc
thatsothoundotctl01-dotcom <thatsothoundotctl01@gmail.com>
2026-09-04 07:28:47 -0700

    engine with sm_ptr

---
+++ new_ranks/pointer/engine.cpp
@@ -0,0 +1,39 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std ; 
+
+class Engine {
+
+  public  : 
+  Engine() {
+    std::cout << "Engine create " <<endl ; 
+
+  } 
+  ~Engine() {
+    std::cout << "Engine des" <<endl ; 
+  } 
+  void Run() {
+    std::cout << "running machine " <<endl ; 
+  }
+} ; 
+class Car {
+
+  private : 
+    std::unique_ptr<Engine> engine ; 
+  public : 
+     Car() : engine(std::make_unique<Engine>()) {}   
+     void start() {
+      
+       engine -> Run() ; 
+     }
+} ; 
+int main() { 
+
+
+  Car s ; 
+  s.start() ; 
+
+  return 0 ; 
+}
