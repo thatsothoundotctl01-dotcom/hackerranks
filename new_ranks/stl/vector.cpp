ed61e7ae632587ce6ff2cb24f27fa29c8965aa90
thatsothoundotctl01-dotcom <thatsothoundotctl01@gmail.com>
2026-09-04 20:01:12 +0700

    vector basic

---
+++ new_ranks/stl/vector.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int main() {
+
+  std::vector<int> size  = {1,2,4} ; 
+   size.push_back(2) ; 
+   for (const int arr : size ) {
+     std::cout << arr <<endl ;  
+   }
+}
