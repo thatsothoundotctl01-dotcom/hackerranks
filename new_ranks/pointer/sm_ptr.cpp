275e859f69555b0499b77c39f19a04b76af6f0bf
thatsothoundotctl01-dotcom <thatsothoundotctl01@gmail.com>
2026-09-04 19:58:57 +0700

    smart_ptr nw c

---
+++ new_ranks/pointer/sm_ptr.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace  std ; 
+
+
+class Us { 
+
+}
+int main() {
+  std::cout  << "Hello world "  <<endl ; 
+  std::unique_ptr<int> s  = std::make_unique<int>(2) ; 
+
+  return 0 ; 
+}
