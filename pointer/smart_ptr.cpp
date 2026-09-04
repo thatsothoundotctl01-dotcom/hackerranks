02d1b98f1918016ee7d2bcd2a663da502ffcdba2
thatsothoundotctl01-dotcom <thatsothoundotctl01@gmail.com>
2026-09-04 19:55:09 +0700

    smart ptr :)

---
+++ pointer/smart_ptr.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ; 
+
+class Sol {
+
+  public  : 
+    string name ; 
+    int age ;  
+    void show () {
+      std::cout<<left ; 
+      std::cout << "name : " << name <<endl ; 
+      std::cout << "Hello  : " << age <<endl ; 
+    }
+} ; 
+int main() {
+
+  std::cout << "Hello world " <<endl ; 
+
+  return 0 ; 
+}
